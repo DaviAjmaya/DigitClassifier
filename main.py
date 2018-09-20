ce218a9ea676cc9ab8f2b77ef0a72f2bc45d0324
Davi Ajmaya <davi.ajmaya@gmail.com>
2018-09-20 13:34:26 +0200

    Refined plots

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
     from tkinter import *
     import tensorflow as tf
-    from PIL import Image, ImageGrab
+    from PIL import Image, ImageGrab, ImageOps
     import numpy as np
     import matplotlib.pyplot as plt
     from scipy import ndimage
@@ -19,7 +19,7 @@
             self.clear_button = Button(self.root, text='Clear', command=self.use_clear)
             self.clear_button.grid(row=0, column=2)
 
-            self.c = Canvas(self.root, bg='black', width=168, height=168)
+            self.c = Canvas(self.root, bg='white', width=168, height=168)
             self.c.grid(row=1, columnspan=3, padx=60)
 
             self.Text = Label(self.root, text="", font=("", 20))
@@ -51,7 +51,7 @@
             line_width = self.choose_size_button.get()
             if self.old_x and self.old_y:
                 self.c.create_line(self.old_x, self.old_y, event.x, event.y,
-                                   width=line_width,fill='white',
+                                   width=line_width,fill='black',
                                    capstyle=ROUND, smooth=TRUE, splinesteps=36)
             self.old_x = event.x
             self.old_y = event.y
@@ -65,6 +65,7 @@
             x1 = x + self.c.winfo_width()-4
             y1 = y + self.c.winfo_height()-4
             img = ImageGrab.grab().crop((x, y, x1, y1)).convert('L')
+            img = ImageOps.invert(img)
 
             # Setup plot
             plt.figure(6, (8, 6))
@@ -75,8 +76,9 @@
 
             # Plot the original image extracted from canvas
             plt.subplot(2, 4, 1)
-            plt.title("Original")
+            plt.title("Inverted")
             plt.imshow(img)
+
 
             # Keep deleting black sides to remove excess black pixels around the
             arr = np.array(img)
@@ -99,29 +101,31 @@
             plt.imshow(cropped)
 
             # Down-scale image while keeping aspect ratio
-            rows, cols = cropped.height, cropped.width
+            downscaled = cropped
+            rows, cols = downscaled.height, downscaled.width
             if rows > cols:
                 factor = 20.0 / rows
                 rows = 20
                 cols = int(round(cols * factor))
-                cropped = cropped.resize((cols, rows), Image.ANTIALIAS)
+                downscaled = downscaled.resize((cols, rows), Image.ANTIALIAS)
             else:
                 factor = 20.0 / cols
                 cols = 20
                 rows = int(round(rows * factor))
-                cropped = cropped.resize((cols, rows), Image.ANTIALIAS)
-
-            # Paste the digit onto a black 28x28 image
-            downscaled = Image.new("L", (28, 28))
-            downscaled.paste(cropped, (4, 4))
+                downscaled = downscaled.resize((cols, rows), Image.ANTIALIAS)
 
             # Plot the downscaled image
             plt.subplot(2, 4, 3)
             plt.title("Downscaled")
             plt.imshow(downscaled)
 
+
+            # Paste the digit onto a black 28x28 image
+            centered = Image.new("L", (28, 28))
+            centered.paste(downscaled, (4, 4))
+
             # Scale pixel values to a range of 0 to 1
-            centered = np.array(downscaled) / 255.0
+            centered = np.array(centered) / 255.0
 
             # Center digit in image using Center of Mass
             c1 = ndimage.center_of_mass(np.ones_like(centered))
@@ -129,6 +133,7 @@
             centered = np.roll(centered, int(round((c1[0] - cm[0]))), axis=0)
             centered = np.roll(centered, int(round((c1[1] - cm[1]))), axis=1)
 
+            # Plot the centered image
             plt.subplot(2, 4, 4)
             plt.title("Centered")
             plt.imshow(centered, cmap='gray')
@@ -145,16 +150,13 @@
             # Get the model's prediction
             prediction = self.model.predict_classes(inp)
             p = self.model.predict(inp)
-
-            print(p)
-            p = np.power(p, (1/10))
+            self.change_text(prediction[0])
 
             # Plot image and prediction rates
-            self.change_text(prediction[0])
             plt.subplot(2, 1, 2)
             plt.title("Probabilities", fontsize=18)
+            p = np.power(p, (1/10))
             plt.xticks(np.arange(len(p[0])))
-            #plt.ylim((0,0.3))
             plt.bar(np.arange(len(p[0])), p[0], 0.2)
             plt.draw()
 
